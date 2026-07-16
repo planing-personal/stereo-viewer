ed8741814055debb34335da05280a6a09775bfb0
planing-personal <97713052+planing-personal@users.noreply.github.com>
2026-07-16 20:07:37 +1000

    Add landing page
    Add landing page.
    Add sample image.

--- samples/samples.js
+++ samples/samples.js
@@ -1,8 +1,14 @@
 const SAMPLES = [
   {
     thumbnail: 'samples/thumbnail1.png',
-    description: 'Sample 1',
+    description: 'Palace, Poland',
     leftImageUrl: 'samples/left1.png',
     rightImageUrl: 'samples/right1.png',
+  },{
+    thumbnail: 'samples/amsterdam_thumbnail.png',
+    description: 'Amsterdam University',
+    leftImageUrl: 'samples/amsterdam_x.png',
+    rightImageUrl: 'samples/amsterdam_y.png',
   },
+
 ];
